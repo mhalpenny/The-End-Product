ca7ebcf2f567a2c1343550d267751e962067b3e2
mhalpenny <matt.halpenny@gmail.com>
2022-06-22 16:12:40 -0400

    Revert "init"
    This reverts commit 0bf3b9f23751dedd5dc03d5c61ec60b9f0205297.

--- js/stream.js
+++
@@ -1,7 +0,0 @@
-	//open stream from VPS
-	var myaudio = new Audio('http://159.203.17.208:8080/listen.mp3');
-	//determines if audio has been initialized
-	let init = false;
-	//streaming status
-	let muted = true;
-
