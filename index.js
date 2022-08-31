113f0852bb4edf6d393095b44b3f81a07724ddf3
mhalpenny <matt.halpenny@gmail.com>
2022-08-31 17:16:35 -0400

    reordering

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-var express = require('express');
-var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json(
-    {test: 'test'}
-  );
-});
+//part of temporary front end
+const imageForm = document.querySelector("#imageForm")
+const imageInput = document.querySelector("#imageInput")
 
-module.exports = router;
+imageForm.addEventListener("submit", async event => {
+  event.preventDefault()
+  const file = imageInput.files[0]
+
+  // get secure url from our server
+  const { url } = await fetch("/s3Url").then(res => res.json())
+  console.log(url)
+
+  // post the image direclty to the s3 bucket
+  await fetch(url, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "multipart/form-data"
+    },
+    body: file
+  })
+
+  const imageUrl = url.split('?')[0]
+  console.log(imageUrl)
+
+  // post requst to my server to store any extra data
+  
+  
+  const img = document.createElement("img")
+  img.src = imageUrl
+  document.body.appendChild(img)
+})
