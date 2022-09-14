3538529340d490b9dcac771e76a800e2c07cbd7a
mhalpenny <matt.halpenny@gmail.com>
2022-09-14 15:46:15 -0400

    new fetch

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -20,17 +20,27 @@ function Gallery(props) {
     console.log(keyName);
 
     //depending on dev or build, connect to the back-end to address s3
-    if (process.env.NODE_ENV !== 'production') {
-      const { url } = await fetch(`http://localhost:8080/s3Url?keyName=${keyName}`).then(res => res.json());
+    // if (process.env.NODE_ENV !== 'production') {
+    //   const { url } = await fetch(`http://localhost:8080/s3Url?keyName=${keyName}`).then(res => res.json());
+    //   setShowSellButton(true);
+    //   setUploadUrl(url);
+    // } else{
+    //   const { url } = await fetch(`https://theendproduct.herokuapp.com/s3Url?keyName=${keyName}`).then(res => res.json());
+    //   setShowSellButton(true);
+    //   setUploadUrl(url);
+    // }
+
+    const { url } = await fetch(`https://theendproduct.herokuapp.com/s3Url?keyName=${keyName}`, {
+      crossDomain:true,
+      method: 'GET',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({
+      })
+    })
+      .then(response => response.json())
       setShowSellButton(true);
-      setUploadUrl(url);
-    } else{
-      const { url } = await fetch(`https://theendproduct.herokuapp.com/s3Url`).then(res => res.json());
-      setShowSellButton(true);
-      setUploadUrl(url);
-    }
+        setUploadUrl(url);
   }
-  // ?keyName=${keyName}
   
   //upload the media to s3 with the given link
   const handleSellClick = async () => {
