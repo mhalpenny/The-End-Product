dd462a34f3686dcbbae6502b14b3cb7239596599
mhalpenny <matt.halpenny@gmail.com>
2023-01-07 21:56:46 -0500

    updated favicon

--- src/components/Login.js
+++ src/components/Login.js
@@ -36,5 +36,4 @@ function Login(props) {
   
 }
 
-// this is a dev test!
 export default Login;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Homepage from "./components/Homepage";
 import Control from "./components/Control";
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
